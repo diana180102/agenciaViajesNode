406586651f35a5640d8ca861a7ced203da913200
Diana Mayorga <diana101210@gmail.com>
2023-08-05 16:36:22 -0500

    Update variables entorno

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-console.log(process.env.DB_HOST);
+// console.log(process.env.DB_HOST);
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: '3307',
+const db = new Sequelize(process.env.MYSQLDATABASE,process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: 'mysql',
     define: {
         timestamps: false
